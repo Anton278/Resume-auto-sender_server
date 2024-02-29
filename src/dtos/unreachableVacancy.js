273ba7a952f0dc586b5d09930005069418fdb902
Anton Nakonechnyi <nakonechnyi.an@gmail.com>
2024-03-01 00:42:53 +0200

    fix uncreachable vacancy dto

--- src/dtos/unreachableVacancy.js
+++ src/dtos/unreachableVacancy.js
@@ -1,8 +1,18 @@
 class UnreachableVacancyDto {
   id;
+  jobBoard;
+  jobTitle;
+  companyName;
+  url;
+  reason;
 
   constructor(vacancy) {
     this.id = vacancy._id;
+    this.jobBoard = vacancy.jobBoard;
+    this.jobTitle = vacancy.jobTitle;
+    this.companyName = vacancy.companyName;
+    this.url = vacancy.url;
+    this.reason = vacancy.reason;
   }
 }
 
